337eaf195d9b50236f8e8bca9d66f66ce1357778
Victor Hoang <vikterhoang@gmail.com>
2022-08-27 23:47:22 -0400

    finish merntwitter

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -3,10 +3,31 @@ const router = express.Router();
 const passport = require("passport")
 const validateTweetInput = require('../../validation/tweets')
 const Tweet = require('../../models/Tweet')
+const mongoose = require('mongoose');
 
+router.get('/', (req, res) => {
+    Tweet.find()
+        .sort({ date: -1 })
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
+});
 
+router.get('/user/:user_id', (req, res) => {
+    Tweet.find({user: req.params.user_id})
+        .then(tweets => res.json(tweets))
+        .catch(err =>
+            res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+        )
+    );
+});
 
-router.get("/test", (req, res) => res.json({ msg: "This is the tweets route" }));
+router.get('/:id', (req, res) => {
+    Tweet.findById(req.params.id)
+        .then(tweet => res.json(tweet))
+        .catch(err =>
+            res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+        );
+});
 
 router.post('/',
     passport.authenticate("jwt", { session: false }),
